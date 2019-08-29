a0f3b26d0367aebe087a216b3348d5e1fcc8bdbe
nisha <n84.menon@gmail.com>
2019-08-28 22:28:37 -0500

    This is testing git commands

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// Testing commit
 var index = require('./routes/index');
 
 var app = express();
